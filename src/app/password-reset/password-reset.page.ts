73775debd30602bf474b6146041b6a89a9d64069
Carlos Gacitua <carlitos.gacitua@gmail.com>
2023-09-04 01:08:54 -0300

    04-09-2023, Se creo carpeta 'Images' dentro de 'assets', Se añadieron cambios en pagina 'password-reset'

--- src/app/password-reset/password-reset.page.ts
+++ src/app/password-reset/password-reset.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-password-reset',
   templateUrl: './password-reset.page.html',
   styleUrls: ['./password-reset.page.scss'],
 })
-export class PasswordResetPage implements OnInit {
+export class PasswordResetPage {
+  nombreUsuario: string = '';
+  nombreUsuarioValido: boolean = false;
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
 
-  ngOnInit() {
+  validarEntrada() {
+    // Agregar validaciones como (a.ejemplo, e.ejemplo, c.ejemplo, etc) y (a.ejemplo@duocuc.cl, e.ejemplo@duocuc.cl, c.ejemplo@duocuc.cl, etc)
+    // En caso de que el 'nombreUsuarioValido' sea true, validar la funcion. En caso contrario, mostrar mensaje de error.
   }
 
+  async recuperarContrasena() {
+    if (this.nombreUsuarioValido) {
+      // En la medida de lo posible, mandar mensaje de correo asimilando que funciona.
+
+      const alert = await this.alertController.create({
+        header: 'Éxito',
+        message: 'Se ha enviado un link para restablecer su contraseña a su correo institucional.',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    } else {
+      // Muestra una alerta de error
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Ingrese un nombre de usuario o correo válido.',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    }
+  }
 }
