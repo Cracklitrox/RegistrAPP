30a8dff4f915c12369d79fc581182e761475eb6c
tiogilipollas <111162091+tiogilipollas@users.noreply.github.com>
2023-09-04 21:31:36 -0300

    Commit 2 Diseño

---
+++ src/app/assistance-page/assistance-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assistance-page',
+  templateUrl: './assistance-page.page.html',
+  styleUrls: ['./assistance-page.page.scss'],
+})
+export class AssistancePagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
