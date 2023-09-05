4d7dfa7894acf5fdcf3881e04c076ea5d18e7038
Carlos Gacitua <carlitos.gacitua@gmail.com>
2023-09-05 12:50:30 -0300

    cambios menores

--- src/app/password-reset/password-reset.page.ts
+++ src/app/password-reset/password-reset.page.ts
@@ -13,18 +13,26 @@ export class PasswordResetPage {
   constructor(private alertController: AlertController) {}
 
   validarEntrada() {
+    // Validacion para comprobar si contiene una letra seguida de un punto
+    const validacionPalabraPunto = /^[a-zA-Z]\.(?!.*@)/;
     // Agregar validaciones como (a.ejemplo, e.ejemplo, c.ejemplo, etc) y (a.ejemplo@duocuc.cl, e.ejemplo@duocuc.cl, c.ejemplo@duocuc.cl, etc)
     // En caso de que el 'nombreUsuarioValido' sea true, validar la funcion. En caso contrario, mostrar mensaje de error.
+    if (this.nombreUsuario = 'a') {
+      this.nombreUsuarioValido === true;
+    } else {
+      this.nombreUsuarioValido === false;
+    }
   }
 
   async recuperarContrasena() {
-    if (this.nombreUsuarioValido) {
+    if (this.nombreUsuarioValido === true) {
       // En la medida de lo posible, mandar mensaje de correo asimilando que funciona.
 
       const alert = await this.alertController.create({
         header: 'Éxito',
         message: 'Se ha enviado un link para restablecer su contraseña a su correo institucional.',
         buttons: ['OK'],
+        // Cuando el usuario salga del mensaje de validacion, dirigir automaticamente a la pagina de logueo.
       });
 
       await alert.present();
