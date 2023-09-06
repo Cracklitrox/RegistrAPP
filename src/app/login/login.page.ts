621581617cb35ab7dca6dbef8f84b7935d8a8992
Carlos Gacitua <carlitos.gacitua@gmail.com>
2023-09-06 07:05:26 -0300

    creacion pagina 'assistance-page' y 'password-reset' ambas con errores

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder
-} from '@angular/forms';
-import { AlertController } from '@ionic/angular';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
 
+// Validador personalizado para el correo institucional
+function correoInstitucionalValidator(control: AbstractControl): { [key: string]: boolean } | null {
+  if (control.value && !control.value.endsWith('@duocuc.cl')) {
+    return { correoInvalido: true };
+  }
+  return null;
+}
+
+// Validador personalizado para la contraseña
+function contrasenaValidator(control: AbstractControl): { [key: string]: boolean } | null {
+  const minLength = 5;
+  const maxLength = 20;
+  const value = control.value || '';
+  
+  // Verifica que la contraseña contenga solo letras, números y el carácter '@'
+  const pattern = /^[a-zA-Z0-9@]+$/;
+
+  if (value.length < minLength || value.length > maxLength || !pattern.test(value)) {
+    return { contrasenaInvalida: true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-login',
@@ -16,35 +33,34 @@ import { AlertController } from '@ionic/angular';
 export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
-  
-  constructor(public fb: FormBuilder,
-    public alertController: AlertController) {
-    this.formularioLogin = this.fb.group ({
-      'nombre': new FormControl("",Validators.required),
-      'password': new FormControl("",Validators.required)
-    });
-   }
 
+  constructor(public fb: FormBuilder, public alertController: AlertController, private navCtrl: NavController) {
+    this.formularioLogin = this.fb.group({
+      'correo': new FormControl('', [Validators.required, correoInstitucionalValidator]),
+      'password': new FormControl('', [Validators.required, contrasenaValidator])
+    });
+  }
+
+  paginaQR() {
+    this.navCtrl.navigateForward('/assistance-page');
+  }
   ngOnInit() {
   }
 
-  async Ingresar(){
-    var f = this.formularioLogin.value;
+  async Ingresar() {
+    const f = this.formularioLogin.value;
 
-    if (this.formularioLogin.invalid){
+    if (this.formularioLogin.invalid) {
       const alert = await this.alertController.create({
         header: 'Datos incompletos',
-        message: 'Tienes que llenar todos los datos',
+        message: 'Tienes que llenar todos los datos correctamente',
         buttons: ['Aceptar']
       });
-  
+
       await alert.present();
       return;
     }
-    var usuario = {
-      nombre: f.nombre,
-      password: f.password
-  }
-
+    this.paginaQR();
+    // Continuar con el procesamiento de inicio de sesión
   }
 }
--- src/app/waiting-page-login/waiting-page-login.page.ts
+++ src/app/waiting-page-login/waiting-page-login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-waiting-page-login',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaitingPageLoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private animationCtrl: AnimationController) { }
 
   ngOnInit() {
+    const delay = 4000;
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, delay);
+  }
+
+  ionViewWillEnter() {
+    const logoContainer = document.querySelector('.logo-container');
+    if (logoContainer) {
+      const enterAnimation = this.animationCtrl.create()
+        .addElement(logoContainer)
+        .duration(500)
+        .fromTo('opacity', '0', '1')
+        .fromTo('transform', 'translateY(10px)', 'translateY(0)');
+
+      enterAnimation.play();
+    }
+  }
+
+  ionViewWillLeave() {
+    const logoContainer = document.querySelector('.logo-container');
+    if (logoContainer) {
+      const leaveAnimation = this.animationCtrl.create()
+        .addElement(logoContainer)
+        .duration(500)
+        .fromTo('opacity', '1', '0');
+
+      leaveAnimation.play();
+    }
   }
 
 }
