792b686bfe87cd3d26da37e13552242e6b846cd8
tiogilipollas <111162091+tiogilipollas@users.noreply.github.com>
2023-09-05 22:20:32 -0300

    Commit 3

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,6 +5,7 @@ import {
   Validators,
   FormBuilder
 } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -16,15 +17,34 @@ export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
   
-  constructor(public fb: FormBuilder) {
-
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController) {
     this.formularioLogin = this.fb.group ({
       'nombre': new FormControl("",Validators.required),
       'password': new FormControl("",Validators.required)
-    })
+    });
    }
 
   ngOnInit() {
   }
 
+  async Ingresar(){
+    var f = this.formularioLogin.value;
+
+    if (this.formularioLogin.invalid){
+      const alert = await this.alertController.create({
+        header: 'Datos incompletos',
+        message: 'Tienes que llenar todos los datos',
+        buttons: ['Aceptar']
+      });
+  
+      await alert.present();
+      return;
+    }
+    var usuario = {
+      nombre: f.nombre,
+      password: f.password
+  }
+
+  }
 }
