96380fb8b835360201ed1135170594ef141c33b9
Carlos Gacitua <carlitos.gacitua@gmail.com>
2023-12-11 06:34:02 -0300

    camara funcionando

--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-import { AlertController } from '@ionic/angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ToastController, LoadingController, Platform } from '@ionic/angular';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-camera',
@@ -8,94 +8,140 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./camera.page.scss'],
 })
 export class CameraPage {
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
 
-  constructor(private alerta: AlertController) { }
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult: string | null = null;
+  loading: HTMLIonLoadingElement | null;
 
-  public scanActive: boolean = false;
-  public result: string | any = null;
-
-  async checkPermission(): Promise<boolean> {
-    try {
-      const status = await BarcodeScanner.checkPermission({ force: true });
-
-      if (status.granted) {
-        BarcodeScanner.showBackground();
-        return true;
-      } else if (status.denied) {
-        this.showAndStopScanner();
-        await this.presentError('Se necesitan los permisos de la camara, vaya a la configuracion de la aplicacion y conceda los permisos necesarios.');
-        return false;
-      } else {
-        this.showAndStopScanner();
-        return false;
-      }
-    } catch (error) {
-      this.showAndStopScanner();
-      this.handleError("Error al conceder los permisos para la camara.");
-      return false;
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform
+  ) {
+    const isInStandaloneMode = () =>
+      'standalone' in window.navigator && window.navigator['standalone'];
+    if (this.plt.is('ios') && isInStandaloneMode()) {
+      console.log('I am a an iOS PWA!');
     }
   }
 
-  async presentError(data: string) {
-    const alert = await this.alerta.create({
-      header: 'No se ha pidod escanear el codigo QR.',
-      message: data,
-      buttons: [{
-        text: 'Ok',
-        role: 'Ok'
-      }]
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+  }
+
+  async startScan() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
     });
-    await alert.present();
+    this.videoElement.srcObject = stream;
+    this.videoElement.setAttribute('playsinline', true);
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
   }
-
-  handleError(errorMessage: string) {
-    console.error(errorMessage);
-    this.presentError(errorMessage);
-  }
-
-  showAndStopScanner(ionContent?: Element | null) {
-    if (ionContent) {
-      ionContent.classList.remove('scanner-active');
-    }
-    BarcodeScanner.showBackground();
-    BarcodeScanner.stopScan();
-  }
-
-  stopScanner() {
-    this.scanActive = false;
-    this.showAndStopScanner();
-  }  
-
-  async startScanner() {
-    try {
-      const ionContent = document.querySelector('body');
-      if (ionContent) {
-        ionContent.classList.add('scanner-active');
+  
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
         this.scanActive = true;
       }
-
-      const allowed = await this.checkPermission();
-
-      if (allowed) {
-        BarcodeScanner.hideBackground();
-        const result = await BarcodeScanner.startScan();
-
-        if (result.hasContent) {
-          const scannedData = result.content;
-
-          if (result.hasContent) {
-            this.result = result.content;
-            this.scanActive = false;
-            BarcodeScanner.hideBackground();
-          }
-        } else {
-          this.showAndStopScanner(ionContent);
-        }
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.showQrToast();
       } else {
-        this.showAndStopScanner(ionContent);
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
       }
-    } catch (error) {
-      this.handleError("No se pudo indentificar el codigo QR, intentelo nuevamente.")
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
     }
+  }
+
+  captureImage() {
+    this.fileinput.nativeElement.click();
+  }
+  
+  handleFile(event: Event) {
+    const files = (event.target as HTMLInputElement).files;
+    if (files !== null) {
+      const file = files.item(0);
+      if (file !== null) {
+        var img = new Image();
+        img.onload = () => {
+          this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+          const imageData = this.canvasContext.getImageData(
+            0,
+            0,
+            this.canvasElement.width,
+            this.canvasElement.height
+          );
+          const code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: 'dontInvert'
+          });
+          if (code) {
+            this.scanResult = code.data;
+            this.showQrToast();
+          }
+        };
+        img.src = URL.createObjectURL(file);
+      }
+    }
+  }  
+
+  async showQrToast() {
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: 'top',
+      buttons: [
+        {
+          text: 'Open',
+          handler: () => {
+            if (this.scanResult !== null) {
+              window.open(this.scanResult, '_system', 'location=yes');
+            }
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
+  reset() {
+    this.scanResult = null;
+  }
+
+  stopScan() {
+    this.scanActive = false;
   }
 }
